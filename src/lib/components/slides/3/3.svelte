63ba3066932474f19534ae624086b554b28370fc
Felix Tjernberg <felix.tjernberg@hotmail.se>
2023-04-23 20:22:26 +0200

    Add slide 3 content

--- src/lib/components/slides/3/3.svelte
+++ src/lib/components/slides/3/3.svelte
@@ -13,10 +13,15 @@
     import uSrcset from './u.png?&avif&srcset'
     // @ts-ignore
     import uMeta from './u.png?&meta'
+    // @ts-ignore
+    import cubeSrcset from './cube.png?&avif&srcset'
+    // @ts-ignore
+    import cubeMeta from './cube.png?&meta'
 </script>
 
-<Layout type="columns" numberOfElements={3}>
+<Layout type="columns" numberOfElements={4}>
     <Picture sources={[{ type: 'avif', srcset: uSrcset }]} meta={uMeta} />
     <Picture sources={[{ type: 'avif', srcset: ccSrcset }]} meta={ccMeta} />
     <Picture sources={[{ type: 'avif', srcset: pSrcset }]} meta={pMeta} />
+    <Picture sources={[{ type: 'avif', srcset: cubeSrcset }]} meta={cubeMeta} />
 </Layout>
